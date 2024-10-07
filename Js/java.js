8b97ca2fb7682316e12a4d3c2f7c4977b920f89b
MelvinVerLia <verdinanmelvin@gmail.com>
2024-10-07 22:05:25 +0700

    commit

--- Js/java.js
+++ Js/java.js
@@ -1,5 +1,27 @@
 const button = document.getElementById("notAvailable");
+const hamburger = document.querySelector(".hamburger");
+const offScreen = document.querySelector(".burger-menu");
+const projects = document.querySelectorAll(".project");
 
-button.addEventListener('click', (e) => {
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  offScreen.classList.toggle('active');
+})
+
+button.addEventListener('click', () => {
   alert("Unavailable Link");
+});
+
+projects.forEach((project) => {
+  const projectInner = project.querySelector('.project-inner');
+
+  project.addEventListener('click', () => {
+    projects.forEach(p => p.classList.remove('active'));
+    project.classList.add('active');
+  });
+
+  projectInner.addEventListener('click', (event) => {
+    event.stopPropagation();
+    project.classList.remove('active');
+  });
 });
