0a500d0f84c0a4b86606b15b7e3a1d0dacc93df7
MelvinVerLia <verdinanmelvin@gmail.com>
2024-10-14 23:51:36 +0700

    changed footer and added icons

--- Js/java.js
+++ Js/java.js
@@ -2,10 +2,28 @@ const button = document.getElementById("notAvailable");
 const hamburger = document.querySelector(".hamburger");
 const offScreen = document.querySelector(".burger-menu");
 const projects = document.querySelectorAll(".project");
+const textElement = document.querySelector('.blink');
+const typingDuration = 2000; 
+const linkedin = document.querySelector(".contact-content2");
+const email = document.querySelector(".contact-content1");
+
+email.addEventListener('click', function() {
+  const email = 'verdinanmelvin@gmail.com'; 
+  const mailtoLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}`;
+  window.open(mailtoLink, '_blank');
+})
+
+setTimeout(() => {
+  textElement.classList.add('finished'); 
+}, typingDuration);
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   offScreen.classList.toggle('active');
+})
+
+linkedin.addEventListener('click', () => {
+  window.open("https://www.linkedin.com/in/melvin-verdinan-mulia/", "_blank");
 })
 
 button.addEventListener('click', () => {
